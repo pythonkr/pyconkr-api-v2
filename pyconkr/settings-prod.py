a19152cbafc87bf520e0c44acebe71a394beb30d
parkseongheum <ab6449@gmail.com>
2023-01-27 23:40:29 +0900

    update: mysql 의존성 추가 및 프로퍼티 지정

--- pyconkr/settings-prod.py
+++ pyconkr/settings-prod.py
@@ -1,1 +1,15 @@
+import os
+
 DEBUG = False
+
+# RDS
+DATABASES = {
+    "default": {
+        "ENGINE": "mysql.connector.django",
+        "NAME": os.getenv("AWS_RDS_DATABASE"),
+        "USER": os.getenv("AWS_RDS_USER_ID"),
+        "PASSWORD": os.getenv("AWS_RDS_PW"),
+        "HOST": os.getenv("AWS_RDS_HOST"),
+        "PORT": os.getenv("AWS_RDS_PORT"),
+    }
+}
