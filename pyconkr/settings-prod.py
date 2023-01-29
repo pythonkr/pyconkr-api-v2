76991b59b89d27c5427f4902ee1b5ab5116d58fc
parkseongheum <ab6449@gmail.com>
2023-01-30 00:38:28 +0900

    lint: 코드 포멧 수정

--- pyconkr/settings-prod.py
+++ pyconkr/settings-prod.py
@@ -15,8 +15,8 @@ DATABASES = {
 }
 
 # django-storages: S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 AWS_S3_ACCESS_KEY_ID = os.getenv("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = os.getenv("AWS_S3_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "pyconkr-api-v2-static"
