5a571a8a245ef67e327b310b117b3ff11973dc9e
parkseongheum <ab6449@gmail.com>
2023-02-25 02:01:30 +0900

    lint

--- sponsor/validators.py
+++ sponsor/validators.py
@@ -5,12 +5,12 @@ from sponsor.models import Sponsor
 
 class SponsorValidater:
     def assert_create(self, sponsor: OrderedDict):
-        target = [
-            self.check_remain_slot(sponsor)
-        ]
+        target = [self.check_remain_slot(sponsor)]
 
     def check_remain_slot(self, sponsor: OrderedDict):
         target_level = sponsor.get("level")
 
-        if target_level.limit <= len(Sponsor.objects.filter(level=target_level, accepted=True)):
+        if target_level.limit <= len(
+            Sponsor.objects.filter(level=target_level, accepted=True)
+        ):
             raise RuntimeError("ERROR: 남은 슬롯 없음")
