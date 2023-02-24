3ffb90ecc2589016bac1e0dabc05a85b025da89c
parkseongheum <ab6449@gmail.com>
2023-02-25 02:23:16 +0900

    update: swagger (drf-spectacular) 추가

--- pyconkr/urls.py
+++ pyconkr/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 import sponsor.routers
+import pyconkr.settings as settings
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
@@ -24,3 +31,19 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("sponsors/", include(sponsor.routers.get_router().urls)),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += [
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        # Optional UI:
+        path(
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
