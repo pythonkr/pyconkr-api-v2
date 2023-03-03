ec4ec1230e8384eb8c698b27b3888f070fcbcf0a
parkseongheum <ab6449@gmail.com>
2023-03-03 21:34:24 +0900

    fix: available 조건 수정

--- sponsor/serializers.py
+++ sponsor/serializers.py
@@ -69,4 +69,4 @@ class SponsorRemainingAccountSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def get_available(obj: SponsorLevel):
-        return True if obj.current_remaining_number < obj.limit else False
+        return True if obj.current_remaining_number > 0 else False
