8e8308a79c9a7369e304496a40c89016e76b57d3
parkseongheum <ab6449@gmail.com>
2023-03-03 21:58:41 +0900

    black

--- sponsor/viewsets.py
+++ sponsor/viewsets.py
@@ -7,9 +7,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from sponsor.models import Sponsor, SponsorLevel
 from sponsor.permissions import IsOwnerOrReadOnly, OwnerOnly
-from sponsor.serializers import (SponsorLevelSerializer, SponsorListSerializer,
-                                 SponsorRemainingAccountSerializer,
-                                 SponsorSerializer)
+from sponsor.serializers import (
+    SponsorLevelSerializer,
+    SponsorListSerializer,
+    SponsorRemainingAccountSerializer,
+    SponsorSerializer,
+)
 from sponsor.validators import SponsorValidater
 
 
