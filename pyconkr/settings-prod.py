9b62a13cec2784616ebde79146b9cb5bf93a03b0
parkseongheum <ab6449@gmail.com>
2023-03-06 03:44:22 +0900

    update: custom storage

--- pyconkr/settings-prod.py
+++ pyconkr/settings-prod.py
@@ -22,8 +22,10 @@ DATABASES = {
 
 # django-storages: S3
 del MEDIA_ROOT
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+
+DEFAULT_FILE_STORAGE = "pyconkr.storage.MediaStorage"
+STATICFILES_STORAGE = "pyconkr.storage.StaticStorage"
+
 AWS_S3_ACCESS_KEY_ID = os.getenv("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = os.getenv("AWS_S3_SECRET_ACCESS_KEY")
 AWS_DEFAULT_REGION = "ap-northeast-2"
