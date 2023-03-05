9b65283d665a1e9743b4772bfe1beb1776fab566
parkseongheum <ab6449@gmail.com>
2023-03-05 18:08:56 +0900

    update: custom storage

--- pyconkr/settings-dev.py
+++ pyconkr/settings-dev.py
@@ -26,8 +26,8 @@ del MEDIA_ROOT
 # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 
-DEFAULT_FILE_STORAGE = MediaStorage
-STATICFILES_STORAGE = StaticStorage
+DEFAULT_FILE_STORAGE = "pyconkr.storage.MediaStorage"
+STATICFILES_STORAGE = "pyconkr.storage.StaticStorage"
 
 AWS_S3_ACCESS_KEY_ID = os.getenv("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = os.getenv("AWS_S3_SECRET_ACCESS_KEY")
