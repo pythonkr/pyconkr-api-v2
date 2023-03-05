92ab08059072b4f03c30b3196566a3b3f7b344c9
Hyeok Min Kwon <khmzxc@naver.com>
2023-03-05 20:03:02 +0900

    Edit API url

--- ticket/urls.py
+++ ticket/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     re_path(r"^conference-ticket-types$", views.get__get_conference_ticket_types),
     re_path(r"^conference-ticket-types/(?P<ticket_type_code>\w+)/check",
             views.get__check_conference_ticket_buyable),
-    re_path(r"^conference-ticket", views.post__add_ticket),
+    re_path(r"^conference-tickets", views.post__add_conference_ticket),
 ]
