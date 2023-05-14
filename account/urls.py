d672251d70683ef7aabb9a998cf2ab2088e8f299
Han Lee | 이한결 <76910100+hanlee55@users.noreply.github.com>
2023-05-14 06:42:59 +0000

    typo: singup -> signup

--- account/urls.py
+++ account/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("account/login/", IdLogin.as_view(), name='account_login'),
     path("account/email/", IdLogin.as_view(), name='account_email'),
     path("account/logout/", Logout.as_view(), name='account_logout'),
-    path("account/singup/", IdLogin.as_view(), name="account_signup"),
+    path("account/signup/", IdLogin.as_view(), name="account_signup"),
     path("auth/github/login/", GitHubLogin.as_view(), name="github_login"),
     path("auth/google/login/", GoogleLogin.as_view(), name="google_login"),
     #path("my-page/payments", MyPage.as_view())
