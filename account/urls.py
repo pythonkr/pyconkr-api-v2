f9a2f8c6a980c0b222471a10449c59a95751d40a
Bae KwonHan <darjeeling@gmail.com>
2023-05-14 12:53:32 +0900

    init my_page payments

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from .views import GitHubLogin, GoogleLogin, MyPage
+from .views import GitHubLogin, GoogleLogin, MyPage, mypage_payments
 
 urlpatterns = [
     path("auth/", include("dj_rest_auth.urls")),
     path("auth/github/login/", GitHubLogin.as_view(), name="github_login"),
     path("auth/google/login/", GoogleLogin.as_view(), name="google_login"),
-    path("my-page/payments", MyPage.as_view())
+    #path("my-page/payments", MyPage.as_view())
+    path("my-page/payments/", mypage_payments)
 ]
--- account/views.py
+++ account/views.py
@@ -3,7 +3,8 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 from django.conf import settings
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from rest_framework.views import APIView
 
 
@@ -20,3 +21,7 @@ class GoogleLogin(SocialLoginView):
 
 class MyPage(APIView):
     pass
+
+@login_required
+def mypage_payments(request):
+    return render(request, 'account_mypage_payments.html')
