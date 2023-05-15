4f632000ab7c8cacc631bdb13b9c004d1042495f
Han Lee | 이한결 <76910100+hanlee55@users.noreply.github.com>
2023-05-15 13:01:08 +0000

    fix: change id-login urls

--- account/urls.py
+++ account/urls.py
@@ -6,10 +6,10 @@ from .views import IdLogin, Logout
 
 urlpatterns = [
     path("auth/", include("dj_rest_auth.urls")),
-    path("account/login/", IdLogin.as_view(), name='account_login'),
-    path("account/email/", IdLogin.as_view(), name='account_email'),
-    path("account/logout/", Logout.as_view(), name='account_logout'),
-    path("account/signup/", IdLogin.as_view(), name="account_signup"),
+    path("accounts/login/", IdLogin.as_view(), name='account_login'),
+    path("accounts/email/", IdLogin.as_view(), name='account_email'),
+    path("accounts/logout/", Logout.as_view(), name='account_logout'),
+    path("accounts/signup/", IdLogin.as_view(), name="account_signup"),
     path("auth/github/login/", GitHubLogin.as_view(), name="github_login"),
     path("auth/google/login/", GoogleLogin.as_view(), name="google_login"),
     #path("my-page/payments", MyPage.as_view())
