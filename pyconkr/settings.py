b5a97125881818e7a46dac08b0f3276f2fb3ab5a
parkseongheum <ab6449@gmail.com>
2023-05-16 01:29:44 +0900

    DEFAULT_AUTHENTICATION_CLASSES 수정

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -207,6 +207,8 @@ REST_FRAMEWORK = {
     # YOUR SETTINGS
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
