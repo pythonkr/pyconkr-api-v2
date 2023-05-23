d5be9a26fedf5db4ef074522191a40bf1b062803
parkseongheum <ab6449@gmail.com>
2023-05-24 01:16:03 +0900

    SessionAuthentication 비활성화

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -208,7 +208,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
