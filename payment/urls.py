ece2eb8e2747ada686b1a49a7ce02d19a5a3725f
parkseongheum <ab6449@gmail.com>
2023-05-25 23:29:55 +0900

    누락된 수정사항 반영

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from payment.views import PortoneWebhookApi, get__generate_payment_key
-
+from payment.views import PortoneWebhookApi, post__generate_payment_key, PaymentSuccessApi
 
 urlpatterns = [
     path("portone/webhook/", PortoneWebhookApi.as_view(), name="portone-webhook"),
-    path("key/", get__generate_payment_key, name="get-payment-key"),
+    path("key/", post__generate_payment_key, name="get-payment-key"),
+    path("success/", PaymentSuccessApi.as_view(), name="payment-success"),
 ]
