9ab964e8c33d86f73393655515a10b6aff0fd04c
parkseongheum <ab6449@gmail.com>
2023-05-29 17:05:39 +0900

    오타 수정

--- payment/urls.py
+++ payment/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("portone/webhook/", PortoneWebhookApi.as_view(), name="portone-webhook"),
     path("key/", post__generate_payment_key, name="get-payment-key"),
     path("success/", PaymentSuccessApi.as_view(), name="payment-success"),
-    path("calcel/", post__cancel_payment, name="cancel-payment"),
+    path("cancel/", post__cancel_payment, name="cancel-payment"),
 ]
