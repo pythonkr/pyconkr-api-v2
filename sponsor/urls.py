527ee30b05bcf321c4fa3cc0f6428f06d822000e
oleveloper <oleveloper@gmail.com>
2023-06-04 16:23:51 +0900

    불필요한 router 코드 삭제

--- sponsor/urls.py
+++ sponsor/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 
 from sponsor.viewsets import SponsorViewSet
-
-router = DefaultRouter()
-router.register("", SponsorViewSet, basename="sponsor")
 
 urlpatterns = [
     path("list/", SponsorViewSet.as_view({"get": "list"})),
