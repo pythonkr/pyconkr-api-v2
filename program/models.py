e28333eb7e04843708016ca0a012f2d464be67b7
parkseongheum <ab6449@gmail.com>
2023-08-01 00:10:19 +0900

    아이돌봄 타입 값 변경

--- program/models.py
+++ program/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 CONFERENCE = "CONFERENCE"
 TUTORIAL = "TUTORIAL"
 SPRINT = "SPRINT"
-CHILD_CARE = "CHILD_CARE"
+CHILDCARE = "CHILDCARE"
 
 
 class Program(models.Model):
@@ -41,7 +41,7 @@ class Program(models.Model):
             (CONFERENCE, "컨퍼런스"),
             (TUTORIAL, "튜토리얼"),
             (SPRINT, "스프린트"),
-            (CHILD_CARE, "아이돌봄"),
+            (CHILDCARE, "아이돌봄"),
         ),
     )
     profile_img = models.ImageField(null=True, blank=True)
