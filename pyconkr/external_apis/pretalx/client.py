d7f7f35215cb71518aca40c71c525599d6a0587a
MU-Software <musoftware@daum.net>
2024-09-07 03:19:41 +0900

    단건 조회 API 추가

--- pyconkr/external_apis/pretalx/client.py
+++ pyconkr/external_apis/pretalx/client.py
@@ -8,7 +8,7 @@ import urllib.parse
 import requests
 from django.conf import settings
 
-from .serializers import PretalxPaginatedSessionSerializer
+from .serializers import PretalxPaginatedSessionSerializer, PretalxSessionSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -39,7 +39,7 @@ class PretalxClient:
             logger.error(traceback.format_exception(e))
             raise PretalxException("Pretalx API 요청에 실패했습니다.") from e
 
-    def retrieve_sessions(self, event_name: str, only_confirmed: bool = True) -> dict:
+    def list_sessions(self, event_name: str, only_confirmed: bool = True) -> dict:
         """세션 목록 조회"""
         endpoint = f"api/events/{event_name}/submissions" + ("?state=confirmed" if only_confirmed else "")
 
@@ -52,6 +52,20 @@ class PretalxClient:
             return parsed_result.validated_data
         except Exception as e:
             raise PretalxException("세션 목록을 가져오지 못했습니다, 잠시 후에 다시 시도해주세요.") from e
+
+    def retrieve_session(self, event_name: str, session_id: int) -> dict:
+        """세션 상세 조회"""
+        endpoint = f"api/events/{event_name}/submissions/{session_id}"
+
+        try:
+            result = self._request("GET", endpoint)
+            result.raise_for_status()
+
+            parsed_result = PretalxSessionSerializer(data=result.json())
+            parsed_result.is_valid(raise_exception=True)
+            return parsed_result.validated_data
+        except Exception as e:
+            raise PretalxException("세션을 가져오지 못했습니다, 잠시 후에 다시 시도해주세요.") from e
 
 
 pretalx_client = PretalxClient()
