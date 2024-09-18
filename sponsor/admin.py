81b1827fdd70e78f71096985e2b1d7f6b7c2b0f0
jungmir <ham4051@gmail.com>
2024-09-18 12:16:41 +0900

    Feat: 어드민 페이지에 후원사 등급 및 등급별 혜택 관리 페이지 추가

--- sponsor/admin.py
+++ sponsor/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from import_export.admin import ImportExportModelAdmin
 
-from sponsor.models import Sponsor, SponsorLevel, Patron
+from sponsor.models import Sponsor, SponsorLevel, Patron, SponsorBenefit, BenefitByLevel
 
 
 class SponsorAdmin(SummernoteModelAdmin):
@@ -21,7 +21,7 @@ class SponsorAdmin(SummernoteModelAdmin):
         "accepted",
         "paid_at",
     )
-    list_filter = ("accepted",)
+    list_filter = ("accepted", "submitted")
     ordering = ("-created_at",)
 
 
@@ -35,13 +35,39 @@ class SponsorLevelAdmin(SummernoteModelAdmin):
         "name",
         "price",
         "limit",
+        "year",
     )
     list_editable = ("order",)
     ordering = ("order",)
-    search_fields = ("name",)
+    search_fields = ("name", "year")
 
 
 admin.site.register(SponsorLevel, SponsorLevelAdmin)
+
+
+class SponsorBenefitAdmin(SummernoteModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "desc",
+        "unit",
+        "year",
+        "is_countable",
+    )
+    ordering = ("-year", "id")
+    search_fields = ("name", "year")
+    list_editable = ("unit", "is_countable")
+
+
+admin.site.register(SponsorBenefit, SponsorBenefitAdmin)
+
+
+class BenefitByLevelAdmin(SummernoteModelAdmin):
+    list_display = ("id", "benefit_id", "level_id", "offer")
+    list_editable = ("offer",)
+
+
+admin.site.register(BenefitByLevel, BenefitByLevelAdmin)
 
 
 class PatronAdmin(SummernoteModelAdmin, ImportExportModelAdmin):
